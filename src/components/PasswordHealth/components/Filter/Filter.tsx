c446d3225ee74dd3f1f1da19799b6fc197e3b730
Vitalii S <137-vitalii.shebela@users.noreply.gitlab.host-h.net>
2021-06-05 12:51:14 +0300

    Fixed old items tab title

--- src/components/PasswordHealth/components/Filter/Filter.tsx
+++ src/components/PasswordHealth/components/Filter/Filter.tsx
@@ -6,6 +6,7 @@ import FilterTab from "./components/FilterTab"
 import './filter-style.scss';
 import itemHasWeakPassword from "~/utils/itemHasWeakPassword";
 import itemHasReusedPassword from "~/utils/itemHasReusedPassword";
+import itemHasOldPassword from "~/utils/itemHasOldPassword";
 
 interface IFilter {
   items: Array<IItem>;
@@ -20,11 +21,15 @@ const Filter: FC<IFilter> = ({items}) => {
     itemHasReusedPassword(item, items) ? (count + 1) : count
   ), 0)
 
+  const oldItemsCount = items.reduce((count, item) => (
+    itemHasOldPassword(item) ? (count + 1) : count
+  ), 0)
+
   return (
     <div className="filter">
       <FilterTab title="Weak" count={weakItemsCount} path={Routes.Weak}/>
       <FilterTab title="Reused" count={reusedItemsCount} path={Routes.Reused}/>
-      <FilterTab title="Old" count={reusedItemsCount} path={Routes.Old} />
+      <FilterTab title="Old" count={oldItemsCount} path={Routes.Old} />
     </div>
   );
 };
