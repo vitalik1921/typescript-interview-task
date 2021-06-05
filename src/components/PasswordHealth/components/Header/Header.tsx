3646d5f4c04a1e4808e2cee02bb3a6eb74b8c350
Vitalii S <137-vitalii.shebela@users.noreply.gitlab.host-h.net>
2021-06-05 12:20:58 +0300

    Fixed logout button

--- src/components/PasswordHealth/components/Header/Header.tsx
+++ src/components/PasswordHealth/components/Header/Header.tsx
@@ -1,25 +1,33 @@
-import {FC} from 'react';
-import {IItem} from "~/services/getUserItems";
-import logout from '../../../../services/logout';
+import { FC } from "react";
+import { useHistory } from "react-router-dom";
+import { Routes } from "~/constants";
+import { IItem } from "~/services/getUserItems";
+import logout from "../../../../services/logout";
 
-import './header-style.scss';
+import "./header-style.scss";
 
 interface IHeader {
   items: Array<IItem>;
   username: string;
 }
 
-const Header: FC<IHeader> = ({items, username}) => {
+const Header: FC<IHeader> = ({ items, username }) => {
+  const { push } = useHistory();
+
+  const handleLogout = async () => {
+    await logout();
+    push(Routes.Root);
+  };
 
   return (
     <div className="header">
       <div className="user-section">
-        <button onClick={logout}>{`Logout ${username}`}</button>
+        <button onClick={handleLogout}>{`Logout ${username}`}</button>
       </div>
       <h1>{`${items.length} Items are vulnerable`}</h1>
       <span>Create new complex passwords to protect your accounts</span>
     </div>
-  )
+  );
 };
 
 export default Header;
--- src/services/logout.ts
+++ src/services/logout.ts
@@ -1,5 +1,10 @@
-const logout = () => {
+import { API } from "~/constants";
+import getUrl from "~/utils/getUrl";
 
+const logout = async () => {
+    const url = getUrl(API.Logout);
+    await fetch(url)
+    localStorage.removeItem("token");
 };
 
 export default logout;
