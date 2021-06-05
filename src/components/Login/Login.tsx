fe14da5164eb593369e02e6067e0bbe9f471d56a
Vitalii S <137-vitalii.shebela@users.noreply.gitlab.host-h.net>
2021-06-05 13:43:20 +0300

    Added a loader to the login page

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,35 +1,40 @@
-import {SyntheticEvent, useState} from 'react';
-import {useHistory} from 'react-router-dom';
-import {Routes} from '~/constants';
-import login from '~/services/login';
-import ErrorBlock from '../ErrorBlock';
+import { SyntheticEvent, useState } from "react";
+import { useHistory } from "react-router-dom";
+import { Routes } from "~/constants";
+import login from "~/services/login";
+import ErrorBlock from "../ErrorBlock";
 
-import './login-style.scss';
+import "./login-style.scss";
 
 const Login = () => {
-  const {push} = useHistory();
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const { push } = useHistory();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState<string>();
 
   const handleSubmit = async (event: SyntheticEvent<HTMLFormElement>) => {
     event.preventDefault();
     setErrorMessage(null);
+    setLoading(true);
 
     try {
       await login(username, password);
       push(Routes.PasswordHealth);
     } catch (error) {
       setErrorMessage("Invalid username or password");
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
     <div className="login-page">
-      <form className="login-form" onSubmit={handleSubmit}>
-        <h1 className="text-center">
-          Password Health
-        </h1>
+      <form
+        className={`login-form ${loading ? "loading" : ""}`}
+        onSubmit={handleSubmit}
+      >
+        <h1 className="text-center">Password Health</h1>
         <input
           value={username}
           onChange={(event) => setUsername(event.target.value)}
@@ -44,13 +49,13 @@ const Login = () => {
           type="password"
           className="input mt-24px"
         />
-        <ErrorBlock error={errorMessage}/>
+        <ErrorBlock error={errorMessage} />
         <button type="submit" className="button mt-24px">
           Login
         </button>
       </form>
     </div>
-  )
+  );
 };
 
 export default Login;
